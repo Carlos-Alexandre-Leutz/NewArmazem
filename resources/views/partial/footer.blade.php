7bdbe3fa5a5e2761c1069c9035294b63c79041a5
Carlos Alexandre Leutz <54986289+Carlos-Alexandre-Leutz@users.noreply.github.com>
2021-05-15 14:01:49 -0300

    home

--- resources/views/partial/footer.blade.php
+++ resources/views/partial/footer.blade.php
@@ -1,130 +1,127 @@
 <footer>
-  <div class="container">
+  <div class="wrapper">
     <div class="row">
       <div class="col-md-6">
-        <h1>Entre em Contato</h1>
+        <p class="tit">QUER MONTAR SEU <br> ESCRITORIO?<br>
+          <span class="label">
+            VAMOS CONVERSAR!
+          </span>
+        </p>
+
       </div>
     </div>
     <div class="row">
       <div class="col-md-12">
-        <p>
-          Utilize o formulário abaixo para entrar em contato!
-        </p>
-        <br>
         <form action="#" onsubmit="return false;">
           <div class="row">
             <div class="col-md-6">
               <div class="form-group d-none">
                 <label for="assunto" class="sr-only">Assunto</label>
                 <span class="assunto">
-                  <input type="text" name="assunto"
-                         value=""
-                         size="40"
-                         class="form-control"
-                         id="assunto"
-                         aria-required="true"
-                         aria-invalid="false"
-                         value="Atendimento"
-                         placeholder="Assunto"/>
-                  </span>
+                  <input type="text" name="assunto" value="" size="40" class="form-control" id="assunto" aria-required="true" aria-invalid="false" value="Atendimento" placeholder="Assunto" />
+                </span>
               </div>
               <div class="form-group">
-                <label for="nome" class="sr-only">Nome Completo</label>
+
                 <span class="nome">
-                  <input type="text" name="nome" value=""
-                         size="40"
-                         class="form-control"
-                         id="nome"
-                         aria-required="true"
-                         aria-invalid="false"
-                         placeholder="Nome Completo"/>
+                  <input type="text" name="nome" value="" size="40" class="input-form" id="nome" aria-required="true" aria-invalid="false" placeholder="   Nome Completo" />
                 </span>
               </div>
               <div class="form-group">
-                <label for="email" class="sr-only">E-mail</label>
+
                 <span class="email">
-                  <input type="email" name="email" value=""
-                         size="40"
-                         class="form-control"
-                         id="email"
-                         aria-required="true"
-                         aria-invalid="false"
-                         placeholder="E-mail"/>
+                  <input type="email" name="email" value="" size="40" class="input-form" id="email" aria-required="true" aria-invalid="false" placeholder="   E-mail" />
                 </span>
               </div>
               <div class="form-group">
-                <label for="telefone" class="sr-only">Telefone</label>
+
                 <span class="telefone">
-                  <input type="tel" name="telefone"
-                         value=""
-                         size="40"
-                         class="form-control"
-                         id="telefone"
-                         aria-required="true"
-                         aria-invalid="false"
-                         placeholder="Telefone"/>
+                  <input type="tel" name="telefone" value="" size="40" class="input-form" id="telefone" aria-required="true" aria-invalid="false" placeholder="   Telefone" />
                 </span>
               </div>
             </div>
             <div class="col-md-6">
               <div class="form-group">
-                <label for="mensagem" class="sr-only">Sua Mensagem</label>
+
                 <span class="mensagem">
-                    <textarea name="mensagem" cols="40"
-                              rows="10"
-                              class="form-control"
-                              id="mensagem"
-                              aria-required="true"
-                              aria-invalid="false"
-                              placeholder="Como podemos te ajudar hoje?"></textarea>
-                 </span>
+                  <textarea name="mensagem" cols="40" rows="6" class="area-form" id="mensagem" aria-required="true" aria-invalid="false" placeholder="   Como podemos te ajudar hoje?"></textarea>
+                </span>
               </div>
             </div>
           </div>
-          <div class="row">
-            <div class="col-md-6 offset-md-6">
+          <div class="row ">
+            <div class="col-md-6 offset-md-6 d-flex justify-content-between">
               <div class="custom-control custom-checkbox" style="font-size: 14px;">
                 <input type="checkbox" class="custom-control-input" id="relacionamento" name="relacionamento">
                 <label class="custom-control-label" for="relacionamento" style="padding-top: 2px;">
                   Desejo receber notícias e promoções no
-                  meu e-mail e WhatsApp.</label>
+                  meu e-mail</label>
               </div>
-              <div class="button-container text-right">
-                <button type="submit" class="btn" id="sendContact"><i class="fas fa-envelope"></i> Enviar</button>
-                {{--<button type="submit" class="btn btn-primary"><i class="fas fa-sync fa-spin"></i> Enviar</button>--}}
+              <div class="cont-btn-enviar">
+                <button type="submit" id="sendContact"><i class="fas fa-envelope"></i> Enviar!</button>
+                {{--<button type="submit" ><i class="fas fa-sync fa-spin"></i> Enviar!</button>--}}
               </div>
             </div>
           </div>
         </form>
-        <div class="pt-5">
-          <address>   <p>
-              <i class="fas fa-phone"></i>
-              (47) 3348-0291
-              (47) 3349-8809
-            </p>
-            <p>
-              <i class="fas fa-at"></i>
-              <a href="mailto:contato@armazemdoescritorio.com.br" title="Clique para enviar email diretamente">contato@armazemdoescritorio.com.br</a>
-            </p>
-            <p>
-              <i class="fas fa-map-marked-alt"></i>
-              <a href="https://goo.gl/maps/r9Q2BZQaMVA2" target="_blank" title="Clique para ver no Google Maps">
-                Rua Samuel Heusi, 73 - 88301-32 <span class="sep">|</span> <span
-                  class="line-break">Centro - Itajaí/SC</span>
-              </a>
-            </p>
-
-          </address>
-        </div>
-        <div class="d-flex justify-content-between copyright py-3">
-          <p>Armazém do Escritório é marca registrada &trade; Copyright 2005
-            - {{ date('Y') }}</p>
-          <div>
-            <a href="https://www.instagram.com/explore/locations/23669937/" target="_blank" class="social instagram" title="Veja nossas fotos no Instagram"><span>Instagram</span></a>
-            <a href="https://www.facebook.com/armazemdoescritorioitajai?fref=ts" target="_blank" class="social facebook" title="Visite nossa página no Facebook"><span>Facebook</span></a>
-          </div>
-        </div>
       </div>
     </div>
   </div>
+  <div class="footer">
+    <div class="wrapper">
+      <img src="{{asset('images/Logo Rodapé.png')}}" alt="Armazém do Escritório">
+    </div>
+    <div class="conteiner-risco-footer"></div>
+    <section class="wrapper cont-footer-button">
+      <div>
+        <a href="">Fale Conosco</a><br>
+        <a href="">Sobre o Armazém</a><br>
+        <a href="">Trabalhe Conosco</a>
+
+      </div>
+      <div>
+        itajaí, SC <br>
+        <a href="https://goo.gl/maps/r9Q2BZQaMVA2" target="_blank" title="Clique para ver no Google Maps">
+          R. Samuel Heusi, 73 Centro <br>
+          <i class="fab fa-whatsapp"></i>
+          <i class="fas fa-phone-alt"></i>
+          (47) 3348-0291
+
+        </a>
+      </div>
+      <div>
+        Balneario Camboriú, SC <br>
+        <a href="https://goo.gl/maps/r9Q2BZQaMVA2" target="_blank" title="Clique para ver no Google Maps">
+          R. Portugal 31, Bairo das Naçoões <br>
+          <i class="fab fa-whatsapp"></i>
+          <i class="fas fa-phone-alt"></i>
+          47 3367 1487
+        </a>
+
+      </div>
+      <div>
+        <div class="contato-email">
+          <a href="mailto:contato@armazemdoescritorio.com.br" title="Clique para enviar email diretamente">contato@armazemdoescritorio.com.br</a>
+        </div>
+        <div class="cont-i-redes">
+          <a href="https://www.instagram.com/explore/locations/23669937/" target="_blank" class="social instagram" title="Veja nossas fotos no Instagram">
+            <i class="fab fa-instagram"></i>
+          </a>
+          <a href="https://www.facebook.com/armazemdoescritorioitajai?fref=ts" target="_blank" class="social facebook" title="Visite nossa página no Facebook">
+            <i class="fab fa-facebook-f"></i>
+          </a>
+        </div>
+
+      </div>
+    </section>
+
+
+    <!--   
+    <p>Armazém do Escritório é marca registrada &trade; Copyright 2005
+      - {{ date('Y') }}</p> -->
+
+
+
+  </div>
+
 </footer>
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -26,6 +26,7 @@
     <!-- JavaScript Bundle with Popper -->
     <script src="https://code.jquery.com/jquery-3.6.0.js" integrity="sha256-H+K7U5CnXl1h5ywQfKtSj8PCmoN9aaq30gDh27Xc0jk=" crossorigin="anonymous"></script>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-eOJMYsd53ii+scO/bJGFsiCZc+5NDVN2yr8+0RDqr0Ql0h+rP48ckxlpbzKgwra6" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css" integrity="sha512-iBBXm8fW90+nuLcSKlbmrPcLa0OT92xO1BIsZ+ywDWZCvqsWgccV3gFoRBv0z+8dLJgyAHIhR35VZc2oM/gI1w==" crossorigin="anonymous" />
 </head>
 
 <body>
@@ -111,77 +112,88 @@
                     </figcaption>
                 </figure>
             </div>
+
+            .
             @empty
-            <div class="">
-                <div class="alert alert-warning">
-                    Não há linhas associadas à esta categoria no momento.<br>
-                    Pedimos a gentileza que verifique conosco o motivo!
-                </div>
+            <div class="alert alert-warning">
+                Não há linhas associadas à esta categoria no momento.<br>
+                Pedimos a gentileza que verifique conosco o motivo!
             </div>
             @endforelse
 
+
     </section>
     <div class="cont-img-contato">
-
         <img src="{{asset('images/Imagem-Banner-Empresa-home.png')}}" alt="Armazém do Escritório">
     </div>
     <section class="wrapper">
         <div class="container-contato">
-
-            <h1 id="targetEmpresa">Armazém do Escritório</h1>
-            <p>O Armazém do Escritório é uma empresa familiar que, com mais de 18 anos
+            <h1 id="targetEmpresa">O Armazém</h1>
+            <p>O Armazém do Escritório é uma empresa familiar que, com mais de 20 anos
                 no mercado, atua com vendas e manutenção de mobiliário para escritórios,
                 componentes e artigos de decoração para ambientes de trabalho, ampla
                 linha de móveis para escritórios desde a recepção até a decoração.
-
             </p>
-
-
             <button type="button" data-bs-toggle="collapse" data-bs-target="#collapseExample" aria-expanded="false" aria-controls="collapseExample">
                 Continue lendo ...
             </button>
 
-
         </div>
-        
-     
-        <div class="collapse" id="collapseExample">
-            <div class="card card-body">
-                <p>
-                    Nosso showroom com mais de 600 m², possui uma grande cartela de
-                    marcas, modelos e cores entre cadeiras e conjuntos de ambientes completos
-                    para apreciação e venda.
-                    Contamos com um depósito de 350 m² para armazenagem de pedidos e
-                    produtos para distribuição além de alguns produtos a pronta entrega,
-                    facilitando ainda mais a montagem dos ambientes.
-                    Somos uma empresa completa e sua necessidade está no nosso estoque!
-                </p>
+    </section>
+    <div class="collapse cont-collapse" id="collapseExample">
+        <div class="wrapper">
+            <p>
+                Nosso showroom com mais de 600 m², possui uma grande cartela de
+                marcas, modelos e cores entre cadeiras e conjuntos de ambientes completos
+                para apreciação e venda.
+                Contamos com um depósito de 350 m² para armazenagem de pedidos e
+                produtos para distribuição além de alguns produtos a pronta entrega,
+                facilitando ainda mais a montagem dos ambientes.
+                Somos uma empresa completa e sua necessidade está no nosso estoque!
+            </p>
 
-                <h1>A História</h1>
-                <p>
-                    A Constituição da empresa Comin Comércio de Móveis Ltda. (CNPJ 00.526.441/0001-21) veio a partir de uma oportunidade de negócio tendo em vista o administrador e representante com mais de 30 anos de experiência em vendas de móveis em geral. A idéia inicial era de apenas um Showroom sem comercialização direta mas a necessidade mudou os rumos e a definição da empresa.
-                    Na época policiais 200 m² trabalhamos com pena com apenas uma linha de móveis e cadeiras que atendia perfeitamente para o momento.
-                    Hoje a empresa conta com aproximadamente 1000 m², sendo 650 m² de showroom com uma ampla linha de móveis para escritório que engloba recepção até os detalhes mais delicados da decoração e um depósito de 350 m² com espaço suficiente para colocar pedidos e produtos a serem distribuídos para o consumidor.
-                </p>
+            <h1>A História</h1>
+            <p>
+                A Constituição da empresa Comin Comércio de Móveis Ltda. (CNPJ 00.526.441/0001-21) veio a partir de uma oportunidade de negócio tendo em vista o administrador e representante com mais de 30 anos de experiência em vendas de móveis em geral. A idéia inicial era de apenas um Showroom sem comercialização direta mas a necessidade mudou os rumos e a definição da empresa.
+                Na época policiais 200 m² trabalhamos com pena com apenas uma linha de móveis e cadeiras que atendia perfeitamente para o momento.
+                Hoje a empresa conta com aproximadamente 1000 m², sendo 650 m² de showroom com uma ampla linha de móveis para escritório que engloba recepção até os detalhes mais delicados da decoração e um depósito de 350 m² com espaço suficiente para colocar pedidos e produtos a serem distribuídos para o consumidor.
+            </p>
 
-                <h1>Os Valores</h1>
-                <p>
-                    A missão da empresa Comin Comércio de móveis Ltda. consiste em atuar de maneira correta no mercado, de forma justa, visando a satisfação dos clientes em relação os produtos e serviços de qualidade oferecidos.
-                    A visão é ter uma empresa reconhecida no ramo de escritório e decoração para escritórios em todo estado de Santa Catarina.
-                    Destaca-se pelos valores éticos e morais mantendo o companheirismo, o espírito de equipe, respeito, honestidade e clareza nas atitudes.
-                </p>
+            <h1>Os Valores</h1>
+            <p>
+                A missão da empresa Comin Comércio de móveis Ltda. consiste em atuar de maneira correta no mercado, de forma justa, visando a satisfação dos clientes em relação os produtos e serviços de qualidade oferecidos.
+                A visão é ter uma empresa reconhecida no ramo de escritório e decoração para escritórios em todo estado de Santa Catarina.
+                Destaca-se pelos valores éticos e morais mantendo o companheirismo, o espírito de equipe, respeito, honestidade e clareza nas atitudes.
+            </p>
 
-                <h1>A Marca</h1>
-                <p>
-                    O nome surgiu da idéia de que em um armazém é possível encontrar uma variedade muito grande de produtos e no Armazém do Escritório, de igual forma, é possível encontrar uma linha bem variado de móveis corporativos, desde linhas convencionais até linhas mais diferenciadas. Pode-se montar uma bela recepção com cadeiras decorativas e decorações variadas, salas de reuniões equipadas e ambiente de trabalho que sigam as normas de ergonomia sem perder a beleza.
-                </p>
+            <h1>A Marca</h1>
+            <p>
+                O nome surgiu da idéia de que em um armazém é possível encontrar uma variedade muito grande de produtos e no Armazém do Escritório, de igual forma, é possível encontrar uma linha bem variado de móveis corporativos, desde linhas convencionais até linhas mais diferenciadas. Pode-se montar uma bela recepção com cadeiras decorativas e decorações variadas, salas de reuniões equipadas e ambiente de trabalho que sigam as normas de ergonomia sem perder a beleza.
+            </p>
+        </div>
 
+    </div>
+
+    <div class="conteiner-risco"></div>
+    <!-- LightWidget WIDGET -->
+    <script src="https://cdn.lightwidget.com/widgets/lightwidget.js"></script>
+    <iframe src="//lightwidget.com/widgets/4d0841f0c0b2599c8dd075ec223896ce.html" scrolling="no" allowtransparency="true" style="width:100%;border:0;overflow:hidden;"></iframe>
+    <div class="conteiner-risco"></div>
+    <div class="laranja"></div>
+    <section class="cont-manutecao">
+        <div class="cont-manutecao-itens wrapper">
+            <div>
+                PROCURANDO MANUTENÇÃO?
+            </div>
+            <div>
+                <button>CONFIRA</button>
+                <div>
+                    COMPLETA LINHA DE <br> PRODUTOS PRODUTOS PARA MANUTENÇÃO
+                </div>
             </div>
         </div>
-
     </section>
-    <div class="conteiner-risco"></div>
-<!-- LightWidget WIDGET --><script src="https://cdn.lightwidget.com/widgets/lightwidget.js"></script><iframe src="//lightwidget.com/widgets/57357dc81ce75c5683a81154784b026c.html" scrolling="no" allowtransparency="true" class="lightwidget-widget" style="width:100%;border:0;overflow:hidden;"></iframe>
+    <div class="laranja"></div>
     @include('partial.footer')
     @include('partial.modal-orcamento')
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta3/dist/js/bootstrap.bundle.min.js" integrity="sha384-JEW9xMcG8R+pH31jmWH6WWP0WintQrMb4s7ZOdauHnUtxwoG2vI5DkLtS3qm9Ekf" crossorigin="anonymous"></script>
